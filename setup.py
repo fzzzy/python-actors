5aa4b9ad221a52527c8d7310e11a3b23e298ff2d
Nick V. <devnull@localhost>
2010-06-17 22:34:15 -0400

    Adding a simply setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name = "python-actors",
+      version = "0.1",
+      long_description = "Python Actors",
+      author='Donovan Preston',
+      packages = ['pyact'],
+      package_dir = {'pyact': 'pyact'},
+    )
